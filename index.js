205b36851c4a9aa549aa213c11e75dc222e0b3ee
nahid6677 <mdrajarr025@gmail.com>
2025-01-07 19:09:06 +0600

    finished

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -33,18 +33,52 @@ async function run() {
     const coffeeCollection = client.db("coffeeDB").collection('coffee');
 
     // CRUD => R
-    app.get("/coffee", async(req,res)=>{
-        const cursor = coffeeCollection.find();
-        const result = await cursor.toArray();
-        res.send(result);
+    app.get("/coffee", async (req, res) => {
+      const cursor = coffeeCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+    // CRUD => 
+    app.get("/coffee/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await coffeeCollection.findOne(query);
+      res.send(result)
     })
 
     // Recive data from client side the post method CRUD => c
-    app.post('/coffee', async(req,res)=>{
-        const newCoffee = req.body;
-        // console.log(newCoffee);
-        const result = await coffeeCollection.insertOne(newCoffee);
-        res.send(result);
+    app.post('/coffee', async (req, res) => {
+      const newCoffee = req.body;
+      // console.log(newCoffee);
+      const result = await coffeeCollection.insertOne(newCoffee);
+      res.send(result);
+    })
+    // CRUD => U
+    app.put("/coffee/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updatedCoffee = req.body;
+      const coffee = {
+        $set: {
+          name: updatedCoffee.name,
+          quantity: updatedCoffee.quantity,
+          supplier: updatedCoffee.supplier,
+          teste: updatedCoffee.teste,
+          catagory: updatedCoffee.catagory,
+          details: updatedCoffee.details,
+          photo: updatedCoffee.photo
+        }
+      }
+      const result = await coffeeCollection.updateOne(query,coffee,options);
+      res.send(result);
+    })
+
+    app.delete("/coffee/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await coffeeCollection.deleteOne(query);
+      res.send(result)
     })
 
 
@@ -59,11 +93,11 @@ run().catch(console.dir);
 
 
 
-app.get('/', (req,res)=>{
-    res.send('Coffee making server is running...')
+app.get('/', (req, res) => {
+  res.send('Coffee making server is running...')
 })
 
-app.listen(port, ()=>{
-    console.log(`Coffee makeing server is running on port: ${port}`)
+app.listen(port, () => {
+  console.log(`Coffee makeing server is running on port: ${port}`)
 })
 
